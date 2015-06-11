c12eeba4469706797451ed4ff2d8838b0dc866ce
Joe Andaverde <joe.andaverde@gmail.com>
2015-06-11 12:29:01 -0500

    Sub par histogram

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,9 +1,23 @@
 module Golf where
+import Data.List
+import Data.Function (on)
 
 skips :: [a] -> [[a]]
 skips x = map (\ y -> [v | (v, i) <- zip x r, mod i y == 0]) r
    where r = [1..(length x)]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima (a:b:c:x) = [b | b > a && b > c] ++ localMaxima (b : c : x)
+localMaxima (a:r@(b:c:_)) = [b | b > a && b > c] ++ localMaxima r
 localMaxima _ = []
+
+count :: Eq a => (a -> Bool) -> [a] -> Int
+count fn = length . filter fn
+
+stars :: [Integer] -> [String]
+stars xs = concat $ reverse $ map (\r -> map (\c -> if count (==c) xs >= r then "*" else " ") cs ++ ["\n"]) [1..m]
+   where a = map (\n -> (count (==n) xs, n)) cs
+         m = fromIntegral $ maximum $ map fst a
+         cs = [0..9]
+
+histogram :: [Integer] -> String
+histogram xs = concat (stars xs ++ ["==========\n0123456789\n"])
